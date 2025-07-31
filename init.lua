19f57dfebdcb4522c2a01c26996a681a1806b61c
Yunusali Abduraxmanov <37039084+khakimjanovich@users.noreply.github.com>
2025-07-31 17:07:46 +0500

    feat: initial setup of neovim configuration with lazy.nvim

---
+++ init.lua
@@ -0,0 +1,34 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"stevearc/conform.nvim",
+		lazy = false,
+		keys = {
+			{
+				"<leader>fm",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			format_on_save = { timeout_ms = 2000, lsp_fallback = true },
+		},
+	},
+})
